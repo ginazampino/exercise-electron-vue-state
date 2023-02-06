1f64904353068017e8e1b6ad9a70626db97eaa3d
Gina <9094115+ginazampino@users.noreply.github.com>
2023-02-06 16:54:10 -0500

    Added toggle button switching

--- src/components/shared/TitleBar.vue
+++ src/components/shared/TitleBar.vue
@@ -2,7 +2,7 @@
     <header id="titlebar">
         <ul id="sidebar-controls">
             <li id="toggle-sidebar">
-
+                
             </li>
         </ul>
         <div id="titlebar-title" class="draggable">
@@ -12,16 +12,16 @@
         </div>
         <ul id="titlebar-controls">
             <li id="toggle-mode" @click="toggleMode">
-                Toggle mode
+                <img :src="getToggleStatus(toggleStatus)"/>
             </li>
             <li id="minimize-app">
-
+                <img src="../../assets/icons/minus.svg" />
             </li>
             <li id="maximize-app">
-                
+                <img src="../../assets/icons/maximize.svg" />
             </li>
             <li id="quit-app">
-
+                <img src="../../assets/icons/x.svg" />
             </li>
         </ul>
     </header>
@@ -31,15 +31,19 @@
     export default {
         data() {
             return {
-                toggleStatus: 'off'
+                toggleStatus: 'left'
             }
         },
         methods: {
             toggleModeIcon() {
-
+                (this.toggleStatus === 'left') ? this.toggleStatus = 'right' : this.toggleStatus = 'left';
             },
             toggleMode() {
+                this.toggleModeIcon();
                 this.$emit('toggle-mode');
+            },
+            getToggleStatus(status) {
+                return '/src/assets/icons/toggle-' + status + '.svg';
             }
         }
     };
