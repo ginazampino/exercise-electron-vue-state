1aaca12b5d4c7fc83500712a22e75c589145e1e2
Gina <9094115+ginazampino@users.noreply.github.com>
2023-02-15 13:09:50 -0500

    Setting the grounds for a11y

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,31 +1,43 @@
 <template>
   <header id="titlebar">
         <ul id="sidebar-controls">
-            <li id="toggle-sidebar">
-                <i class="fa-regular fa-arrow-left-to-line"></i>
+            <li>
+                <button id="toggle-sidebar" aria-label="Toggle sidebar">
+                    <i class="fa-regular fa-arrow-left-to-line" alt="Left arrow icon"></i>
+                </button>
             </li>
         </ul>
-        <div id="titlebar-title" class="draggable">
-            <i class="fa-regular fa-stars"></i>
+        <div id="titlebar-title" class="draggable" aria-label="Move window">
+            <i class="fa-regular fa-stars" alt="Stars icon"></i>
             <h1>
                 Concetto
             </h1>
         </div>
         <ul id="titlebar-controls">
-            <li id="toggle-mode" @click="modeStore.toggleMode">
-                <i :class="`fa-regular fa-toggle-${ modeStore.modeIcon }`"></i>
+            <li>
+                <button id="toggle-mode" @click="modeStore.toggleMode" aria-label="Toggle brightness">
+                    <i :class="`fa-regular fa-toggle-${ modeStore.modeIcon }`" alt="Switch icon"></i>
+                </button>
             </li>
-            <li id="toggle-color" @click="themeStore.toggleColor">
-                <i class="fa-regular fa-paint-roller"></i>
+            <li>
+                <button id="toggle-color" @click="themeStore.toggleColor" aria-label="Toggle theme">
+                    <i class="fa-regular fa-paint-roller" alt="Paint roller icon"></i>
+                </button>
             </li>
-            <li id="minimize-app">
-                <i class="fa-regular fa-minus"></i>
+            <li>
+                <button id="minimize-app" aria-label="Minimize window">
+                    <i class="fa-regular fa-minus" alt="Minus icon"></i>
+                </button>
             </li>
-            <li id="maximize-app">
-                <i class="fa-regular fa-expand"></i>
+            <li>
+                <button id="maximize-app" aria-label="Maximize window">
+                    <i class="fa-regular fa-expand" alt="Expand icon"></i>
+                </button>
             </li>
-            <li id="quit-app">
-                <i class="fa-regular fa-xmark"></i>
+            <li>
+                <button id="quit-app" aria-label="Quit application">
+                    <i class="fa-regular fa-xmark" alt="Close icon"></i>
+                </button>
             </li>
         </ul>
     </header>
